53fa5ed49dbb5480f1ef4548aa9aeca69e2ee59b
bhargava <bhargava.kolasani1985@gmail.com>
2023-02-05 19:26:53 -0600

    Initial commit

---
+++ src/test/java/com/kbc/practice/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbc.practice.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
